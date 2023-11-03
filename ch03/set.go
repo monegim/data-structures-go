53d1817202c36e389c23b6a1ba0e305e347bd91f
Mostafa Negim <mostafa.negim@gmail.com>
2023-11-03 18:52:53 +0330

    Add: AddElement

--- ch03/set.go
+++ ch03/set.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Set[T comparable] struct {
 	t_map map[T]bool
 }
@@ -8,6 +10,22 @@ func (set *Set[T]) New() {
 	set.t_map = make(map[T]bool)
 }
 
-func main() {
+func (set *Set[T]) ContainsElement(element T) bool {
+	_, exists := set.t_map[element]
+	return exists
+}
 
+func (set *Set[T]) AddElement(element T) {
+	if !set.ContainsElement(element) {
+		set.t_map[element] = true
+	}
+}
+
+func main() {
+	set := Set[int]{}
+	set.New()
+	element := 2
+	fmt.Println("does the set have the element: ", set.ContainsElement(element))
+	set.AddElement(element)
+	fmt.Println("initial set: ", &set)
 }
