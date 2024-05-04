2ea515605caed9994df87cec8b9702551681bf75
Mostafa Negim <mostafa.negim@gmail.com>
2024-05-04 16:03:09 +0330

    Add: preOrderTraverseTree

--- ch04/binary_search_tree.go
+++ ch04/binary_search_tree.go
@@ -58,6 +58,21 @@ func inOrderTraverseTree(treeNode *TreeNode, function func(int)) {
 		inOrderTraverseTree(treeNode.rightNode, function)
 	}
 }
+
+func (tree *BinarySearchTree) PreOrderTraverseTree(function func(int)) {
+	tree.lock.Lock()
+	defer tree.lock.Unlock()
+	preOrderTraverseTree(tree.rootNode, function)
+}
+
+//  preOrderTraverseTree method
+func preOrderTraverseTree(treeNode *TreeNode, function func(int)) {
+	if treeNode != nil {
+		function(treeNode.value)
+		preOrderTraverseTree(treeNode.leftNode, function)
+		preOrderTraverseTree(treeNode.rightNode, function)
+	}
+}
 func main() {
 	rootNode := &TreeNode{
 		key:   15,
@@ -67,10 +82,15 @@ func main() {
 		rootNode: rootNode,
 	}
 	bst.Insert(6, 6)
+	bst.Insert(23, 23)
+	bst.Insert(71, 71)
+	bst.Insert(50, 50)
 	bst.Insert(4, 4)
 	bst.Insert(7, 7)
 	bst.Insert(5, 5)
-	bst.Insert(23, 23)
+	fmt.Println("Order")
 	bst.InOrderTraverseTree(func(i int) { fmt.Println(i) })
+	fmt.Println("PreOrder")
+	bst.PreOrderTraverseTree(func(i int) { fmt.Println(i) })
 
 }
